ea76e099f7e8b7a39dc6196fbd554f3c77aa658e
houyu <1205157517@qq.com>
2016-09-06 00:01:01 +0800

    agent.py change

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -78,7 +78,7 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=2000)  # run for a specified number of trials
